69c3fd326c2dfd7c23b39206ce9409b111ccaaf8
weixuefeng <weixuefeng@diynova.com>
2021-10-21 15:34:26 +0800

    update lights

---
+++ 3d-demo/pages/components/camera.js
@@ -0,0 +1,12 @@
+import React, { useRef, useEffect } from 'react';
+import { useThree, useFrame } from '@react-three/fiber';
+
+const Camera = (props) => {
+    const ref = useRef();
+    const set = useThree((state) => state.set);
+    useEffect(() => void set({ camera: ref.current }), []);
+    useFrame(() => ref.current.updateMatrixWorld());
+    return <perspectiveCamera ref={ref} {...props} />;
+  };
+
+export default Camera;
--- 3d-demo/pages/scene/main.tsx
+++ 3d-demo/pages/scene/main.tsx
@@ -1,36 +1,75 @@
 import { Canvas } from "@react-three/fiber";
 import { Sky, PointerLockControls,Stars } from "@react-three/drei"
-import { VRCanvas, DefaultXRControllers, Hands, useXR } from "@react-three/xr"
 import { Ground } from "../components/ground";
-import { DirectionalLightProps, DirectionalLight}  from "../components/light"
-import React from "react";
+import Lights  from "../components/light"
+import React, { useState, useEffect, Suspense } from "react";
 import { Physics } from "@react-three/cannon"
 import { Player } from "../components/player"
 import {Building} from "../components/builds/build"
+import Art from '../components/builds/art'
+import Furniture from '../components/builds/Furniture'
+import Camera from '../components/camera'
+import Moon from '../components/moon'
+import * as THREE from 'three';
+
 
 export default function MainScene() {
+	const [night, setNight] = useState(true)
+	const [performance, setPerformance] = useState(true)
+  
+	useEffect(() => {
+	  const handleKeyDown = (e: KeyboardEvent) => {
+		switch(e.code) {
+		  case "KeyN":
+			setNight(!night)
+			return;
+		  case "KeyP":
+			setPerformance(!performance)
+			return;
+		  default: return;
+		}
+	  }
+  
+	  document.addEventListener("keydown", handleKeyDown)
+	  return () => {
+		document.removeEventListener("keydown", handleKeyDown)
+	  }
+	}, [night, performance])  
 
+	
 	return (
 		<div>
-			<Canvas style={{height: "992px"}}>
-				<Sky sunPosition={[50, 80, 100]} />
-				<Stars />
-				<ambientLight intensity={0.5} />
-				<DirectionalLight
-					position={[50, 80, 100]}
-					target={[-5, -3, 50]}
-					intensity={0.5}
-					color={"white"}
-					shadowCamBot={-50}
-					shadowCamTop={50}
-					shadowCamL={53}
-					shadowCamR={-53}
-				/>
-				<fog attach="fog" args={["#f0f4f5", 30, 250]}/>
+			<Canvas style={{height: "992px"}}
+				onCreated={({ gl }) => { 
+					gl.shadowMap.enabled = true
+					gl.shadowMap.type = THREE.PCFSoftShadowMap
+				}}
+			>
+				{/* <Camera fov={60} /> */}
+				{night ? 
+					<>
+						<Stars />
+						<Suspense fallback={null}>
+							<Moon />
+						</Suspense>
+						<fog attach="fog" args={["#272730", 30, 250]}/>
+					</>
+					: 
+					<>
+						<Sky sunPosition={[110, 170, -250]} /> 
+						<fog attach="fog" args={["#f0f4f5", 30, 250]}/>
+					</>
+				}
+				   <Lights 
+						night={night}
+						performance={performance}
+						/>
 				<Physics gravity={[0, -20, 0]}>
 					<Ground/>
 					<Building/>
-
+					<Art/>
+					<Furniture/>
+					<Player/>
 				</Physics>
 				<PointerLockControls />
 			</Canvas>
