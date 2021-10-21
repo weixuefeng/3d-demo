e8dfa67d5103045d125f6035e522c350edf08c47
weixuefeng <weixuefeng@diynova.com>
2021-10-21 16:38:16 +0800

    move dir

--- 3d-demo/pages/index.tsx
+++ 3d-demo/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import React, { useState, useEffect, PointerEventHandler } from 'react'
-import Loading from './components/loading/Loading'
+import Loading from '../components/loading/Loading'
 
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import MainScene from './scene/main'
+import MainScene from '../scene/main'
 
 const Home: NextPage = () => {
   const [ready, setReady] = useState(false)
